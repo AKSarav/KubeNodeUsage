58427196f8b42e992fc4fdd8fbae679d8d549221
Sarav <sarasoft@live.com>
2024-06-19 13:45:21 +0530

    Moving the Input as a Type to Utils.

---
+++ utils/base.go
@@ -0,0 +1,13 @@
+package utils
+
+type Inputs struct {
+	HelpFlag     bool
+	ReverseFlag  bool
+	Debug        bool
+	SortBy       string
+	FilterNodes  string
+	FilterColor  string
+	FilterLabels string
+	Metrics      string
+	LabelToDisplay string
+}
